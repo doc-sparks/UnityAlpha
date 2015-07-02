933eeaa4dd4f2ce6dac84a119cd725449aa206d4
doc-sparks <slatermw@EPSC02PN49MFVH8.local>
2015-07-02 21:25:37 +0100

    Completed basic state machine

--- Assets/Scripts/Actors/MovementController.cs
+++ Assets/Scripts/Actors/MovementController.cs
@@ -4,55 +4,128 @@ using System.Collections;
 public class MovementController : MonoBehaviour {
 
 	public float moveSpeed_ = 12.0f;
+	public float jumpForceHorizontal_ = 750f;
+	public float jumpForceVertical_ = 1250f;
+	public float jumpingDelay_ = 0.1f;
 
-	private float jumpForceHorizontal_ = 500f;
-	private float jumpForceVertical_ = 1250f;
-	private float jumpingDelay_ = 0.1f;
 	private float jumpTime_ = 0f;
+	private StateMachine stateMachine_ = null;
 
 	// Use this for initialization
 	void Start () {
 	
+		// We have states so grab it if there is one
+		stateMachine_ = GetComponent<StateMachine>();
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
 	
 		// NOTE: have changed the gravity option in the Input Manager to make it stop straight away
+		// NOTE: keyboard input and actual moving is seperated as with a state machine, could be triggered
+		//			by something else
+
 		// check for keyboard input
 		Vector3 scale = transform.localScale;
-		if (Input.GetAxis ("Horizontal") < 0f) {
-			// move left
-			transform.Translate( new Vector3(moveSpeed_ * -1f * Time.deltaTime, 0f, 0f) );
 
-			// point image in correct direction
-			if (scale.x > 0f)
-			{
-				scale.x *= -1f;
-				transform.localScale = scale;
+		// flag to set the stop_moving state
+		bool no_move = true;
+
+		// ------------------------------------------------
+		// Move Left
+		bool move_left = false;
+		if (Input.GetAxis ("Horizontal") < 0f) {
+
+			// check the state machine if given
+			if (stateMachine_) {
+				stateMachine_.changeState(StateMachine.State.left);
+				no_move = false;
 			}
 
-		} else if (Input.GetAxis ("Horizontal") > 0f) {
-			// move right
-			transform.Translate( new Vector3(moveSpeed_ * Time.deltaTime, 0f, 0f) );
+			// set the flag in case we're not running in a state machine
+			move_left = true;
+		}
+
+		// change flag based on state machine if present
+		if (stateMachine_) {
+			move_left = (stateMachine_.currState_ == StateMachine.State.left);
+		}
+
+		// Now actually do the moves
+		if (move_left) {
+
+			// move left
+			transform.Translate (new Vector3 (moveSpeed_ * -1f * Time.deltaTime, 0f, 0f));
 
 			// point image in correct direction
-			if (scale.x < 0f)
-			{
+			if (scale.x > 0f) {
 				scale.x *= -1f;
 				transform.localScale = scale;
 			}
 		}
 
-		// separately check for jumping
-		if (Time.time > jumpTime_)
-		{
-			jumpTime_ = Time.time + jumpingDelay_;
-		
-			if (Input.GetAxis ("Jump") > 0.0f){
-				Rigidbody2D body = GetComponent<Rigidbody2D>();
-				body.AddForce( new Vector2( 0.0f * jumpForceHorizontal_, jumpForceVertical_ ) );
+		// ------------------------------------------------
+		// Move Right
+		bool move_right = false;
+		if (Input.GetAxis ("Horizontal") > 0f) {
+			
+			// check the state machine if given
+			if (stateMachine_) {
+				stateMachine_.changeState(StateMachine.State.right);
+				no_move = false;
 			}
+			
+			// set the flag in case we're not running in a state machine
+			move_right = true;
+		}
+		
+		// change flag based on state machine if present
+		if (stateMachine_) {
+			move_right = (stateMachine_.currState_ == StateMachine.State.right);
+		}
+		
+		// Now actually do the moves
+		if (move_right) {
+			
+			// move left
+			transform.Translate (new Vector3 (moveSpeed_ * 1f * Time.deltaTime, 0f, 0f));
+			
+			// point image in correct direction
+			if (scale.x < 0f) {
+				scale.x *= -1f;
+				transform.localScale = scale;
+			}
+		}
+
+		// ------------------------------------------------
+		// sort out no horizontal movement
+		if ((no_move) && (stateMachine_)) {
+			stateMachine_.changeStateTimed ( StateMachine.State.stop_moving, -1f, StateMachine.State.idle );
+		}
+
+		// ------------------------------------------------
+		// separately check for jumping
+		bool jump_flag = false;
+		if ((Time.time > jumpTime_) && (Input.GetAxis ("Jump") > 0.0f)) {
+
+			// check for the state machine
+			if (stateMachine_) {
+				stateMachine_.changeState (StateMachine.State.jumping);
+			}
+
+			// set the flag in case we're not running in a state machine
+			jump_flag = true;
+		}
+
+		// change flag based on state machine if present
+		if (stateMachine_) {
+			jump_flag = (stateMachine_.currState_ == StateMachine.State.jumping);
+		}
+
+		if (jump_flag) {
+			jumpTime_ = Time.time + jumpingDelay_;
+			Rigidbody2D body = GetComponent<Rigidbody2D>();
+			body.AddForce( new Vector2( Input.GetAxis ("Horizontal") * jumpForceHorizontal_, jumpForceVertical_ ) );
 		}
 	}
 	/*
---
+++ Assets/Scripts/Actors/StateMachine.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StateMachine : MonoBehaviour {
+
+	public enum State {
+		idle = 0,
+
+		left,
+		right,
+		stop_moving,
+
+		jumping,
+	}
+	
+	public State currState_ = State.idle;
+	public State nextState_ = State.idle;
+	public float stateTimer_ = -1f;
+
+	// ------------------------------------------------
+	// allowed state changes
+	private Dictionary<State, List<State> > allowed_state_change_;
+
+	// Use this for initialization
+	void Start () {
+
+		// set up the state change array
+		allowed_state_change_ = new Dictionary<State, List<State> > ();
+
+		// set of states you can go to from this state
+		allowed_state_change_[ State.idle ] = new List<State>();
+		allowed_state_change_[ State.idle ].Add(State.left);
+		allowed_state_change_[ State.idle ].Add(State.right);
+		//allowed_state_change_[ State.idle ].Add(State.jumping);
+	
+		allowed_state_change_[ State.left ] = new List<State>();
+		allowed_state_change_[ State.left ].Add(State.right);
+		//allowed_state_change_[ State.left ].Add(State.jumping);
+		allowed_state_change_[ State.left ].Add(State.stop_moving);
+
+		allowed_state_change_[ State.right ] = new List<State>();
+		allowed_state_change_[ State.right ].Add(State.left);
+		//allowed_state_change_[ State.right ].Add(State.jumping);
+		allowed_state_change_[ State.right ].Add(State.stop_moving);
+
+		allowed_state_change_[ State.stop_moving ] = new List<State>();
+		allowed_state_change_[ State.stop_moving ].Add(State.left);
+		allowed_state_change_[ State.stop_moving ].Add(State.right);
+		//allowed_state_change_[ State.stop_moving ].Add(State.jumping);
+		allowed_state_change_[ State.stop_moving ].Add(State.idle);
+
+		allowed_state_change_[ State.jumping ] = new List<State>();
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	
+		// have we got a timed state to switch to?
+		if ((stateTimer_ > 0f) && (Time.time > stateTimer_) &&
+		    (allowed_state_change_ [currState_].Contains (nextState_)) ) {
+				currState_ = nextState_;
+		}
+	}
+
+	// attempt to change the state
+	public bool changeState(State new_state) {
+
+		// check if we can go to the new state from the current one
+		if (allowed_state_change_ [currState_].Contains (new_state)) {
+			currState_ = new_state;
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	// attempt to change the state but set a timer to change back/to second state
+	public bool changeStateTimed( State new_state, float t, State next_state){
+
+		// check if we can go to the new state from the current one
+		if (!allowed_state_change_ [currState_].Contains (new_state)) {
+			return false;
+		} 
+
+		// set the variables to trigger the next state
+		currState_ = new_state;
+		stateTimer_ = Time.time + t;
+		nextState_ = next_state;
+		return true;
+	}
+}
